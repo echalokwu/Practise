692ac82d0bccca63d7c24375e04eeac6057a8653
echalokwu <echalokwu@gmail.com>
2020-03-04 22:53:43 +0100

    changes made to framework

--- DemoGuru/test_DemoGuru.py
+++ DemoGuru/test_DemoGuru.py
@@ -19,38 +19,36 @@ class TestGuru:
         driver.close()
         driver.quit()
 
+    """
+    Test Case1: Verify Item in Mobile List page can be sorted by Name
+    ----------------------------------------------------------------
+    1) Goto http://live.demoguru99.com/
+    2) Verify Title of the page "THIS IS DEMO SITE
+    3) Click on MOBILE menu
+    4) Verify Tile of the page "MOBILE
+    5) In the list of all mobile, select 'SORT BY' dropdown as 'name'
+    6) Verify all products are sorted by name
+    """
+    @pytest.mark.skip("Testing")
     def test_case_1(self, test_setUp):
-        """Verify Item in Mobile List page can be sorted by Name"""
-
-        # Step 1. Goto http://live.demoguru99.com/
         driver.get("http://live.demoguru99.com/")
-
-        # Step 2. Verify Title of the page "THIS IS DEMO SITE"
         actual_title = driver.title
         expected_title = "THIS IS DEMO SITE"
         try:
             assert expected_title == actual_title
         except AssertionError:
             print(actual_title, "is the correct title")
-
-        # Step 3. Click on MOBILE menu
         driver.find_element_by_xpath("//a[contains(text(),'Mobile')]").click()
-
-        # Step 4. Verify Tile of the page "MOBILE"
         actual_title2 = driver.title
         expected_title2 = "MOBILE"
         try:
             assert expected_title2 == actual_title2
         except AssertionError:
             print(actual_title2, "is the correct title")
-
-        # Step 5. In the list of all mobile, select 'SORT BY' dropdown as 'name'
         element = driver.find_element_by_xpath("(//SELECT[@onchange='setLocation(this.value)'])[1]")
         drp = Select(element)
         drp.select_by_visible_text("Name")
         time.sleep(3)
-
-        # Step 6. Verify all products are sorted by name
         x = "name"
         try:
             assert (x in driver.page_source)
@@ -59,26 +57,24 @@ class TestGuru:
         except AssertionError:
             print("All products are not sorted by", x)
 
+    """
+       Test Case2: Verify that cost of product in list page and details page are equal
+       ----------------------------------------------------------------
+       1) Goto http://live.demoguru99.com/
+       2) Click on MOBILE menu
+       3) In the list of all mobile, read the cost of Sony Xperia mobile. Note this value $100
+       4) Click on Sony Xperia mobile
+       5) Read the Sony Xperia mobile from the detail page
+       6) Compare value in Step 3 & 5
+       """
     def test_case_2(self, test_setUp):
-        """Verify that cost of product in list page and details page are equal"""
-        # Step 1. Goto http://live.demoguru99.com/
         driver.get("http://live.demoguru99.com/")
-
-        # Step 2. Click on MOBILE menu
         driver.find_element_by_xpath("//a[contains(text(),'Mobile')]").click()
-
-        # Step 3. In the list of all mobile, read the cost of Sony Xperia mobile. Note this value $100
         XperiaPrice = driver.find_element_by_id("product-price-1")
         print(XperiaPrice.text)
-
-        # Step 4. Click on Sony Xperia mobile
         driver.find_element_by_id("product-collection-image-1").click()
-
-        # Step 5. Read the Sony Xperia mobile from the detail page
         detailPrice = driver.find_element_by_xpath("//span[@class='price']")
         print(detailPrice.text)
-
-        # Step 6. Compare value in Step 3 & 5
         try:
             assert XperiaPrice == detailPrice
             print("Verified Successfully")
