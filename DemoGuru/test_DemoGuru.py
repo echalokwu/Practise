231a95b72972d9e5fa3cde2c627dafed395b37ab
echalokwu <echalokwu@gmail.com>
2020-03-06 00:01:08 +0100

    changes made to framework

--- DemoGuru/test_DemoGuru.py
+++ DemoGuru/test_DemoGuru.py
@@ -29,7 +29,7 @@ class TestGuru:
     5) In the list of all mobile, select 'SORT BY' dropdown as 'name'
     6) Verify all products are sorted by name
     """
-    @pytest.mark.skip("Testing")
+    @pytest.mark.skip
     def test_case_1(self, test_setUp):
         driver.get("http://live.demoguru99.com/")
         actual_title = driver.title
@@ -67,6 +67,7 @@ class TestGuru:
        5) Read the Sony Xperia mobile from the detail page
        6) Compare value in Step 3 & 5
        """
+
     def test_case_2(self, test_setUp):
         driver.get("http://live.demoguru99.com/")
         driver.find_element_by_xpath("//a[contains(text(),'Mobile')]").click()
@@ -80,6 +81,37 @@ class TestGuru:
             print("Verified Successfully")
         except AssertionError:
             print("Step 3 and Step 5 not equal")
+
+    """
+           Test Case3: Verify that you cannot add more product in cart than product available in store
+           ----------------------------------------------------------------
+           1) Goto http://live.demoguru99.com/
+           2) Click on MOBILE menu
+           3) In the list of all mobile, click on ADD TO CART for Sony Xperia mobile
+           4) Change QTY value to 1000 and click UPDATE button
+           5) Verify the error message
+           6) Then click on EMPTY CART link in the footer of list of all mobiles
+           7) Verify cart is empty
+           """
+
+    def test_case_3(self, test_setUp):
+        driver.get("http://live.demoguru99.com/")
+        driver.find_element_by_xpath("//a[contains(text(),'Mobile')]").click()
+        driver.find_element_by_xpath("(//SPAN[text()='Add to Cart'][text()='Add to Cart'])[1]").click()
+        time.sleep(3)
+        QTY = driver.find_element_by_xpath("//*[@id='shopping-cart-table']/tbody/tr/td[4]/input")
+        QTY.clear()
+        QTY.send_keys("1000")
+        Update = driver.find_element_by_xpath("//td[@class='product-cart-actions']//button[@name='update_cart_action']")
+        Update.click()
+        expectedError = 'The requested quantity for "Sony Xperia" is not available'
+        actualError = driver.find_element_by_xpath("//p[@class='item-msg error']").text
+        try:
+            assert expectedError == actualError
+            print(actualError)
+        except AssertionError:
+            print(actualError, "is the correct error displayed")
+
 
 
 
